4cfd410a38f8566f6c31452d8b52de723eec7f48
WOJCIECH PAWLIK <LADMQH34DT@ad.ing.net>
2017-02-23 08:29:52 +0100

    1.0 telefony

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+﻿function Telefon(marka, cena, kolor, power) {
+	this.marka = marka; 
+	this.cena = cena;
+	this.kolor = kolor;
+	this.power = power;
+}
+
+Telefon.prototype.printInfo = function() {
+	console.log("To jest telefon marki " + this.marka + ", kosztuje " + this.cena + " złotych i ma kolor " + this.kolor);
+};
+
+Telefon.prototype.printFunction = function() {
+	console.log("Power function dla tego telefonu: " + this.power);
+};
+
+var iPhone6S = new Telefon("Apple", 2250, "srebrny", "Touch ID");
+var onePlus = new Telefon("OnePlus", 1800, "czarny", "6GB RAM");
+var galaxyS = new Telefon("Samsung", 2190, "złoty", "Wybucha");
+
+iPhone6S.printInfo();
+iPhone6S.printFunction();
+
+onePlus.printInfo();
+onePlus.printFunction();
+
+galaxyS.printInfo();
+galaxyS.printFunction();
